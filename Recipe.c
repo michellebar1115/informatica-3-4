8de20ca3983993d8ec22eb155eacdef5432806f1
michellebar1115 <michellebar1115@gmail.com>
2026-04-20 18:19:26 +0000

    Recipe

---
+++ Recipe.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    printf("Coconut Cranberry Dessert \n");
+    printf("\n");
+    printf("1 1/2 cups\t Graham cracker crumbs \n");
+    printf("1/2 cup\t Butter or margarine,melted \n");
+    printf("1 can\t Sweetened condensed milk \n");
+    printf("1 cup\t Flaked coconut \n");
+    printf("1 cup\t Dried cranberries \n");
+    printf("2 cups\t Vanilla chips \n");
+    printf("1 cup\t Sliced almonds or pecans \n");
+    printf("\n");
+}
