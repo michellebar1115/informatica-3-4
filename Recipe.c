31211e1f91044dd260cebb05e83bfe6e15238429
michellebar1115 <michellebar1115@gmail.com>
2026-04-21 21:54:04 +0000

    Coconut Cranberry Dessert

--- Recipe.c
+++ Recipe.c
@@ -6,11 +6,14 @@ int main(void)
     printf("Coconut Cranberry Dessert \n");
     printf("\n");
     printf("1 1/2 cups\t Graham cracker crumbs \n");
-    printf("1/2 cup\t Butter or margarine,melted \n");
-    printf("1 can\t Sweetened condensed milk \n");
-    printf("1 cup\t Flaked coconut \n");
-    printf("1 cup\t Dried cranberries \n");
-    printf("2 cups\t Vanilla chips \n");
-    printf("1 cup\t Sliced almonds or pecans \n");
+    printf("1/2 cup\t\t Butter or margarine,melted \n");
+    printf("1 can\t\t Sweetened condensed milk \n");
+    printf("1 cup\t\t Flaked coconut \n");
+    printf("1 cup\t\t Dried cranberries \n");
+    printf("2 cups\t\t Vanilla chips \n");
+    printf("1 cup\t\t Sliced almonds or pecans \n");
     printf("\n");
+    printf("1. In a large bowl mix the mayonnaise, yogurt, curry powder, chicken, grapes, and chopped vegetables.\n");
+    printf("2. Arrange rolls on a baking sheet and heat at 325°F for 5 minutes or until slightly crispy.\n");
+    printf("3. Cut a thin slice from the top of each roll and hollow out the inside of the rolls. Fill each roll with salad mixture and replace the roll tops. Refrigerate until serving.\n");
 }
