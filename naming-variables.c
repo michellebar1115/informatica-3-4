d1973e2487a8768a772802f0aa8ca9a299942225
michellebar1115 <michellebar1115@gmail.com>
2026-04-21 21:42:01 +0000

    Naming Variables

---
+++ naming-variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+int number; //I removed exclamation mark
+int testvalue; //I remove spaces
+int example1; //I can't start with a number
+float int_; //It needs a dif name than int, I added a _
+printf("Variables created successfully.\n");
+}// I added curly brackets
