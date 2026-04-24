7955a213e1181188bac22ae5e64bcb30df06d3ee
michellebar1115 <michellebar1115@gmail.com>
2026-04-24 14:44:31 +0000

    hello

--- hello.c
+++ hello.c
@@ -2,5 +2,13 @@
 
 int main(void)
 {
-    printf("Hello Michelle! \n");
+    char Name[50];
+    printf("Your name: ");
+    scanf ("%s", &Name);
+    printf("Hello %s! \n", Name);
+
+    char Color[50];
+    printf("Favorite color: ");
+    scanf("%s", &Color);
+    printf("Your favorite color is: %s \n.", Color);
 }
