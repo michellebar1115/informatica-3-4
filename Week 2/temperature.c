f9de3145e5a0f45d8508fac569d5c19d2573c912
michellebar1115 <michellebar1115@gmail.com>
2026-04-27 16:56:20 +0000

    temperature

--- Week 2/temperature.c
+++ Week 2/temperature.c
@@ -2,12 +2,13 @@
 
 int main(void)
 {
-    int F
+    int F;
     printf("Fahrenheit= ");
-    scanf("%d°",&F);
+    scanf("%d",&F);
 
-    int celcius = F-32/1.8;
+    int celcius = (F-32)/1.8;
     printf("Celcius = %d° \n", celcius);
-    
+    printf("%d°F = %d°C\n",F,celcius);
+// -40°F is the same as -40°C.
 }
 
