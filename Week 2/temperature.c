af954a70924c32729cef6a0a0f830921b5482e60
michellebar1115 <michellebar1115@gmail.com>
2026-04-27 14:59:01 +0000

    temperature

---
+++ Week 2/temperature.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int F
+    printf("Fahrenheit= ");
+    scanf("%d°",&F);
+
+    int celcius = F-32/1.8;
+    printf("Celcius = %d° \n", celcius);
+    
+}
+
