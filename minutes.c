cf3e4f076459626513a132b417ca34c5d93a1c02
michellebar1115 <michellebar1115@gmail.com>
2026-04-30 15:40:31 +0000

    minutes

--- minutes.c
+++ minutes.c
@@ -6,15 +6,18 @@ int main(void)
     printf("Movie running time (in minutes): ");
     scanf("%d", &minutes);
 
-    int start;
-    printf("Start time= ");
-    scanf("%d", &start);
+    int startHour;
+    int startMin;
+    printf("Start time: ");
+    scanf("%d:%d", &startHour,&startMin);
 
     int hour = minutes/60;
     int min = minutes%60;
-    printf("The movie will last %d hr and %d min \n", hour,min);
 
-    int end= start+hour;
-    printf("End time= %d:%d\n",end,min");
+    int endHour= startHour+hour;
+    int endMin= startMin+min;
+
+    printf("The movie will last %d hr and %d min\n", hour,min);
+    printf("End time: %d:%d\n",endHour,endMin);
 
 }
